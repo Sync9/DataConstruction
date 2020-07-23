16390f0a09a70226170a9b47e3ad988b2dabdb1f
Sync9 <50036595+Sync9@users.noreply.github.com>
2020-07-24 00:29:42 +0800

    Update OrderedList.cpp

--- LinearList/OrderedList.cpp
+++ LinearList/OrderedList.cpp
@@ -1,1 +1,34 @@
-
+#include <iostream>
+typedef struct OrderedList{
+    int* head;
+    int length;
+    int size;
+};
+OrderedList init(int block);
+int main() {
+    using namespace std;
+    int input=0;
+    cout<<"Please input the number of the element in the ordered list"<<endl;
+    cin>>input;
+    OrderedList orderedList=init(input);
+    cout<<"Please input the values of the element in the ordered list"<<endl;
+    for(int i=0;i<orderedList.length;i++){
+        cin>>orderedList.head[i];
+    }
+    cout<<"The size of the ordered list is "<<orderedList.size<<" Bytes"<<endl;
+    cout<<"The values of every element in the ordered list are as follows"<<endl;
+    for(int i=0;i<orderedList.length;i++){
+        cout<<orderedList.head[i]<<endl;
+    }
+    cout<<"The addresses of every element in the ordered list are as follows"<<endl;
+    for(int i=0;i<orderedList.length;i++){
+        cout<<&orderedList.head[i]<<endl;
+    }
+}
+OrderedList init(int block){
+    OrderedList orderedList;
+    orderedList.head=(int*)malloc(sizeof(int)*block);
+    orderedList.length=block;
+    orderedList.size=orderedList.length*sizeof(int);
+    return orderedList;
+}
